85faa74c05e1c25b312549cdd1056271c842377a
sanketsarode-a <sdsarode2812@gmail.com>
2024-12-04 20:19:18 +0530

    Update backend.tf

--- POC-terraform/env/prod/backend.tf
+++ POC-terraform/env/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-aws-poc-001"  # Replace with your S3 bucket name
+    bucket         = "configuration000001"  # Replace with your S3 bucket name
     key            = "POC-terraform/env/prod/terraform.tfstate"  # State file path
-    region         = "ap-south-1"                  # Replace with your AWS region
+    region         = "us-east-1"                  # Replace with your AWS region
     encrypt        = true                           # Encrypt state file
-# Optional, for state locking
+
   }
 }
