20889dc928914243208b60a3ab1f7870ebc3ee7c
sanketsarode-a <sdsarode2812@gmail.com>
2024-12-04 20:21:00 +0530

    Delete POC-terraform/env/prod/backend.tf

--- POC-terraform/env/prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "configuration000001"  # Replace with your S3 bucket name
-    key            = "POC-terraform/env/prod/terraform.tfstate"  # State file path
-    region         = "us-east-1"                  # Replace with your AWS region
-    encrypt        = true                           # Encrypt state file
-
-  }
-}
